710f1da48abebf7c50c9d242eb5151eb857793db
Tommy Noe <tjnoe01@gmail.com>
2020-02-10 23:36:07 -0500

    add x and o onclick handler

--- src/Cell.ts
+++ src/Cell.ts
@@ -3,10 +3,13 @@ import {
   useNewComponent,
   useDraw,
   Geometry,
-  Polygon
+  Polygon,
+  SystemFont,
+  Label,
+  Mouse
 } from "@hex-engine/2d";
 
-export default function Cell({ size, position }) {
+export default function Cell({ size, position, getContent, onClick }) {
   useType(Cell);
 
   useNewComponent(() =>
@@ -16,9 +19,19 @@ export default function Cell({ size, position }) {
     })
   );
 
+  const font = useNewComponent(() =>
+    SystemFont({ name: "sans-serif", size: size.y })
+  );
+
+  const label = useNewComponent(() => Label({ font }));
+  const mouse = useNewComponent(Mouse);
+  mouse.onClick(onClick);
+
   useDraw(context => {
     context.lineWidth = 1;
     context.strokeStyle = "black";
     context.strokeRect(0, 0, size.x, size.y);
+    label.text = getContent();
+    label.draw(context);
   });
 }
--- src/Root.ts
+++ src/Root.ts
@@ -14,9 +14,11 @@ export default function Root() {
   const canvas = useNewComponent(() => Canvas({ backgroundColor: "white" }));
   canvas.fullscreen();
 
-  const grid = new Grid(3, 3, " ");
+  const grid = new Grid<string>(3, 3, " ");
   const cellSize = new Vector(16, 16);
   const firstCellPosition = new Vector(100, 100);
+
+  let state = "PLACING_X";
 
   for (const [rowIndex, columnIndex] of grid.contents()) {
     useChild(() =>
@@ -24,7 +26,28 @@ export default function Root() {
         size: cellSize,
         position: firstCellPosition
           .addX(cellSize.x * rowIndex)
-          .addY(cellSize.y * columnIndex)
+          .addY(cellSize.y * columnIndex),
+        getContent: () => grid.get(rowIndex, columnIndex),
+        onClick: () => {
+          switch (state) {
+            case "PLACING_X": {
+              const content = grid.get(rowIndex, columnIndex);
+              if (content === " ") {
+                grid.set(rowIndex, columnIndex, "x");
+                state = "PLACING_O";
+              }
+              break;
+            }
+            case "PLACING_O": {
+              const content = grid.get(rowIndex, columnIndex);
+              if (content === " ") {
+                grid.set(rowIndex, columnIndex, "o");
+                state = "PLACING_X";
+              }
+              break;
+            }
+          }
+        }
       })
     );
   }
